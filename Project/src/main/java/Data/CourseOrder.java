f132a2668065d51513833ec48acc5033a5db0cb1
nilssonjj <ejeino-7@student.ltu.se>
2020-01-21 09:04:26 +0100

    Added CourseOrder

---
+++ Project/src/main/java/Data/CourseOrder.java
@@ -0,0 +1,80 @@
+package Data;
+
+public class CourseOrder {
+
+	private Course[][] courses;
+	private int readingPeriods;
+	
+	// How many courses can run in parallel?
+	private final int COURSES_PER_PERIOD = 4;
+	
+	/**
+	 *
+	 * @param readingPeriods Amount of reading periods
+	 */
+	public CourseOrder(int readingPeriods) {
+		this.courses = new Course[COURSES_PER_PERIOD][readingPeriods];
+		this.readingPeriods = readingPeriods;
+	}
+	
+	
+	/**
+	 * 	Sets course at the given position
+	 * 
+	 * @param course
+	 * @param period
+	 * @param pos
+	 */
+	public void setCourseAt(Course course, int period, int pos) {
+		if(period < this.readingPeriods && pos < COURSES_PER_PERIOD) {
+			
+			this.courses[pos][readingPeriods] = course;
+			
+		}
+	}
+	
+	/**
+	 * Removes the course from the course array
+	 * @param course
+	 */
+	public void removeCourse(Course course) {
+		
+		for(int i = 0; i < COURSES_PER_PERIOD; i++) {
+			for(int j = 0; j < this.readingPeriods; j++) {
+				if(this.courses[i][j].courseCode == course.courseCode) {
+					this.courses[i][j] = null;
+					
+					// Break the loops
+					i = COURSES_PER_PERIOD;
+					j = this.readingPeriods;
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * Deletes the course from and replaces it with the course to
+	 * 
+	 * @param from
+	 * @param to
+	 */
+	public void changeCourse(Course from, Course to) {
+		for(int i = 0; i < COURSES_PER_PERIOD; i++) {
+			for(int j = 0; j < this.readingPeriods; j++) {
+				if(this.courses[i][j].courseCode == from.courseCode) {
+					this.courses[i][j] = to;
+					
+					// Break the loops
+					i = COURSES_PER_PERIOD;
+					j = this.readingPeriods;
+				}
+			}
+		}
+	}
+	
+	public Course[][] getCourseArray() {
+		return this.courses;
+	}
+	
+}
