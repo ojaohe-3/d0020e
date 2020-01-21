3902d21b7243826c15b8f218fbb148892673e429
Wilma <Wilma@DESKTOP-DFNP8B8>
2020-01-21 09:23:50 +0100

    Created classes for Course and KC

---
+++ Project/src/main/java/Data/KC.java
@@ -0,0 +1,62 @@
+package Data;
+
+public class KC {
+	private int id;
+	private int taxonomyLevel;
+	private String[] name;
+	private String[] generalDescription;
+	private String[] taxonomyDescription;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 * @param id
+	 * @param generalDescription
+	 * @param taxonomyLevel
+	 * @param taxonomyDescription
+	 */
+	public KC(String[] name, int id, String[] generalDescription, int taxonomyLevel, String[] taxonomyDescription) {
+		this.name = name;
+		this.id = id;
+		this.generalDescription = generalDescription;
+		this.taxonomyLevel = taxonomyLevel;
+		this.taxonomyDescription = taxonomyDescription;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public int getTaxonomyLevel() {
+		return taxonomyLevel;
+	}
+
+	public void setTaxonomyLevel(int taxonomyLevel) {
+		this.taxonomyLevel = taxonomyLevel;
+	}
+
+	public String[] getName() {
+		return name;
+	}
+
+	public void setName(String[] name) {
+		this.name = name;
+	}
+
+	public String[] getGeneralDescription() {
+		return generalDescription;
+	}
+
+	public void setGeneralDescription(String[] generalDescription) {
+		this.generalDescription = generalDescription;
+	}
+
+	public String[] getTaxonomyDescription() {
+		return taxonomyDescription;
+	}
+
+	public void setTaxonomyDescription(String[] taxonomyDescription) {
+		this.taxonomyDescription = taxonomyDescription;
+	}
+}
