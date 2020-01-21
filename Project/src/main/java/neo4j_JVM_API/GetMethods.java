b1c087290574752eec063f245092ca080094508d
nilssonjj <ejeino-7@student.ltu.se>
2020-01-21 09:54:10 +0100

    Split API

---
+++ Project/src/main/java/neo4j_JVM_API/GetMethods.java
@@ -0,0 +1,37 @@
+package neo4j_JVM_API;
+
+import neoCommunicator.Neo4jCommunicator;
+
+public class GetMethods {
+	
+	private final Neo4jCommunicator communicator;
+	
+	public GetMethods(Neo4jCommunicator communicator){
+		this.communicator = communicator;
+	}
+	
+	public String[] getTopics() {
+		return null;
+	}
+	
+	public boolean login() {
+		return false;
+	}
+	
+	public String getUser() {
+		return "";
+	}
+	
+	public String getProgram() {
+		return "";
+	}
+	public void getCourse() {
+		
+	}
+	
+	public void getKCwithTaxonomyLevel() {
+		
+	}
+	
+	
+}
--- Project/src/main/java/neo4j_JVM_API/Neo4JAPI.java
+++ Project/src/main/java/neo4j_JVM_API/Neo4JAPI.java
@@ -13,100 +13,44 @@ import neoCommunicator.Neo4jCommunicator;
  *
  */
 public class Neo4JAPI {
+	
 	private final Neo4jCommunicator communicator;
+	
+	public GetMethods getMethods;
+	public SetMethods setMethods;
+	public ModifyMethods modifyMethods;
 	
 	
 	public Neo4JAPI(String uri, String user, String password) {
 		this.communicator = new Neo4jCommunicator(uri, user, password);
+		this.getMethods = new GetMethods(communicator);
+		this.setMethods = new SetMethods(communicator);
+		this.modifyMethods = new ModifyMethods(communicator);
 	}
 	
-	public void CreateCourse() {
-		
-	}
 	
-	public void createKC () {
-		
-	}
+}
 	
-	public void createKCgroup() {
-		
-	}
 	
-	public void createProgram() {
-		
-	}
 	
-	public void removeCourse() {
-		
-	}
 	
-	public void removeKCtaxonomy() {
-		
-	}
 	
-	public void removeKCByName() {
-		
-	}
 	
-	public void removeProgram() {
-		
-	}
 	
-	public void editCourse() {
-		
-	}
 	
-	public void editKCWithTaxonomyLevel() {
-		
-	}
 	
-	public void editProgram() {
-		
-	}
 	
-	public void getCourse() {
-		
-	}
 	
-	public void getKCwithTaxonomyLevel() {
-		
-	}
 	
-	public String getProgram() {
-		return "";
-	}
 	
-	public void addUser() {
-		
-	}
 	
-	public void removeUser() {
-		
-	}
 	
-	public void changeUserPrivileges() {
-		
-	}
 	
-	public String getUser() {
-		return "";
-	}
 	
-	public boolean login() {
-		return false;
-	}
 	
-	public void addTopic() {
-		
-	}
 	
-	public void removeTopic() {
-		
-	}
 	
-	public String[] getTopics() {
-		return null;
-	}
+	
 	
 	
 }
