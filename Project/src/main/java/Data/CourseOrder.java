1175cc933fa13b775498bd9a0161e5ac4db76f05
Wilma <Wilma@DESKTOP-DFNP8B8>
2020-01-24 08:55:49 +0100

    Updated course order

--- Project/src/main/java/Data/CourseOrder.java
+++ Project/src/main/java/Data/CourseOrder.java
@@ -34,6 +34,39 @@ public class CourseOrder {
 	}
 	
 	/**
+	 * Help function to find first empty position in 2d array
+	 * 
+	 * @param readingPeriods
+	 * @return
+	 */
+	private int findFirstPos(int readingPeriods) {
+		for(int i = 0; i < this.COURSES_PER_PERIOD; i++) {
+			if(this.courses[i][readingPeriods] == null) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * Set course in course order
+	 * 
+	 * @param course
+	 * @param period
+	 * @return true if success else false
+	 */
+	public boolean setCourseAtLp(Course course, int period) {
+		if(period < this.readingPeriods) {
+			int pos = findFirstPos(period);
+			if(pos != -1) {
+				this.courses[pos][readingPeriods] = course;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
 	 * Removes the course from the course array
 	 * @param course
 	 */
