731ae77e27309f6c92b68e512aabdfe51583383c
Wilma <Wilma@DESKTOP-DFNP8B8>
2020-01-24 08:18:01 +0100

    Added one relations enum

--- Project/src/main/java/Data/Relations.java
+++ Project/src/main/java/Data/Relations.java
@@ -2,7 +2,8 @@ package Data;
 
 public enum Relations {
 	DEVELOPED("developed"),
-	REQUIRED("required");
+	REQUIRED("required"),
+	BELONGS_TO("belongsTo");
 	private String relation;
 	private Relations(String relation) {
 		this.relation = relation;
