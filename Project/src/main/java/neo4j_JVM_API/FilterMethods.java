ce155ab115402d6d1a910cc81853dfef2baf3e28
Wilma <Wilma@DESKTOP-DFNP8B8>
2020-01-24 11:19:38 +0100

    Started on filtering

---
+++ Project/src/main/java/neo4j_JVM_API/FilterMethods.java
@@ -0,0 +1,122 @@
+package neo4j_JVM_API;
+
+import java.util.ArrayList;
+
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.StatementResult;
+
+import Data.Course.CourseLabels;
+import neoCommunicator.Neo4jCommunicator;
+
+public class FilterMethods {
+	private final Neo4jCommunicator communicator;
+	
+	public FilterMethods(Neo4jCommunicator communicator){
+		this.communicator = communicator;
+	}
+	
+	/**
+	 * Filter course by code
+	 * 
+	 * @param code
+	 * @return array with matching course code
+	 */
+	public String[] filterCourseByCode(String code) {
+		String query = "MATCH (course: Course) WHERE course.courseCode STARTS WITH \"" + code + "\"";
+		query += "RETURN course";
+		
+		StatementResult result = this.communicator.readFromNeo(query);
+		ArrayList<String> courses = new ArrayList<String>();
+		while(result.hasNext()) {
+			courses.add(result.next().get("course").get("courseCode").toString());
+		}
+		
+		return courses.toArray(new String[courses.size()]);
+	}
+	
+	/**
+	 * Filter course by name
+	 * 
+	 * @param code
+	 * @return array with matching course name
+	 */
+	public String[] filterCourseByName(String name) {
+		String query = "MATCH (course: Course) WHERE course.name STARTS WITH \"" + name + "\"";
+		query += "RETURN course";
+		
+		StatementResult result = this.communicator.readFromNeo(query);
+		ArrayList<String> courses = new ArrayList<String>();
+		while(result.hasNext()) {
+			courses.add(result.next().get("course").get("name").toString());
+		}
+		
+		return courses.toArray(new String[courses.size()]);
+	}
+	
+	/**
+	 * Filter course program by code
+	 * 
+	 * @param code
+	 * @return array with matching program
+	 */
+	public String[] filterProgramByCode(String code) {
+		String query = "MATCH (courseProgram: CourseProgram) WHERE courseProgram.code STARTS WITH \"" + code + "\"";
+		query += "RETURN courseProgram";
+		
+		StatementResult result = this.communicator.readFromNeo(query);
+		ArrayList<String> programs = new ArrayList<String>();
+		while(result.hasNext()) {
+			programs.add(result.next().get("courseProgram").get("code").toString());
+		}
+		
+		return programs.toArray(new String[programs.size()]);
+	}
+	
+	/**
+	 * Filter course program by name
+	 * 
+	 * @param code
+	 * @return array with matching program
+	 */
+	public String[] filterProgramByName(String name) {
+		String query = "MATCH (courseProgram: CourseProgram) WHERE courseProgram.name STARTS WITH \"" + name + "\"";
+		query += "RETURN courseProgram";
+		
+		StatementResult result = this.communicator.readFromNeo(query);
+		ArrayList<String> programs = new ArrayList<String>();
+		while(result.hasNext()) {
+			programs.add(result.next().get("courseProgram").get("name").toString());
+		}
+		
+		return programs.toArray(new String[programs.size()]);
+	}
+	
+	/**
+	 * Filter topic
+	 * 
+	 * @param code
+	 * @return array with matching topics
+	 */
+	public String[] filterTopic(String name) {
+		String query = "MATCH (topic: Topic) WHERE topic.name STARTS WITH \"" + name + "\"";
+		query += "RETURN topic";
+		
+		StatementResult result = this.communicator.readFromNeo(query);
+		ArrayList<String> topics = new ArrayList<String>();
+		while(result.hasNext()) {
+			topics.add(result.next().get("topics").get("name").toString());
+		}
+		
+		return topics.toArray(new String[topics.size()]);
+	}
+	
+	/**
+	 * Filter kc
+	 * 
+	 * @param code
+	 * @return array with matching kcs
+	 */
+	public void filterKC() {
+		
+	}
+}
