375f34ba7e458fa77c7055d42b90240efed3056b
ojaohe-3 <ojaohe-3@student.ltu.se>
2020-01-30 14:41:47 +0100

    jsp files

--- Project/src/main/java/com/servlet/TeacherServlet.java
+++ Project/src/main/java/com/servlet/TeacherServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class indexServlet extends HttpServlet {
+public class TeacherServlet extends HttpServlet {
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         super.service(req, res);
