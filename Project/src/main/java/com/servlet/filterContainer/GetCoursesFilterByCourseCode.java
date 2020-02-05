4f0115d92559d7081161cc79e990b73c4d407ced
Wilma <Wilma@DESKTOP-DFNP8B8>
2020-02-05 14:18:19 +0100

    Added filter by code

--- Project/src/main/java/com/servlet/filterContainer/GetCoursesFilterByCourseCode.java
+++ Project/src/main/java/com/servlet/filterContainer/GetCoursesFilterByCourseCode.java
@@ -7,7 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import Data.Course;
+import Data.Course.CourseLabels;
+import Data.CourseInformation;
 import neo4j_JVM_API.Neo4JAPI;
 
 @WebServlet("/GetCourses/FilterByCourseCode")
@@ -20,9 +26,25 @@ public class GetCoursesFilterByCourseCode extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+		CourseInformation[] courses = Neo4JAPI.filterMethods.filterCourseByTag(CourseLabels.CODE, request.getParameter("filter"));
+			
+		try {
+			JSONArray array = new JSONArray();
+			for (CourseInformation course : courses) {
+			    JSONObject obj = new JSONObject();
+			    obj.put(CourseLabels.NAME.toString(), course.getName());
+			    obj.put(CourseLabels.CODE.toString(), course.getCourseCode());
+			    obj.put(CourseLabels.YEAR.toString(), course.getStartPeriod().getYear());
+			    obj.put(CourseLabels.LP.toString(), course.getStartPeriod().getPeriod());
+			    obj.put(CourseLabels.CREDIT.toString(), course.getCredit());
+			    obj.put(CourseLabels.EXAMINER.toString(), course.getExaminer());
+			    array.put(obj);
+			}
+			response.setContentType("text/json");
+			response.getWriter().write(array.toString());
+		} catch (JSONException exception) {}
 		
-		response.setContentType("text/plain");
-		response.getWriter().write("This is the response from FilterByCourseCode");
+		
 		
 	}
 	
