48245e5a12c66d92cdf2d0d7b064aaa9d2c135cb
nilssonjj <ejeino-7@student.ltu.se>
2020-02-07 08:59:25 +0100

    working..

--- Project/WebContent/JavascriptFolder/filterContainerFunctions.js
+++ Project/WebContent/JavascriptFolder/filterContainerFunctions.js
@@ -3,9 +3,18 @@
  */
 
 
-function courseClicked() {
+function courseClicked(code, year, lp) {
 	
-	
+	$.ajax({
+
+		url : 'GetCourse/byCodeAndDate',
+		data : {
+			courseCode : 
+		},
+		success : function(response) {
+			
+		}
+	});
 }
 
 function programClicked() {
